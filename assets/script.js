8dc20d84d9b65959362f4fa7843da8228d62f1eb
mattkellyirl <matthew.kelly@digicom.net.au>
2024-01-14 13:09:20 +1030

    restructured script to load without errors

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,31 @@
 // Variable Declarations
 var questions = [
     {
-        title: "Q1. Commonly used data types do not include:",
+        title: "Commonly used data types do not include:",
         choices: ["Strings", "Alerts", "Booleans", "Numbers"],
         answer: "Alerts" 
 
     },
     {
-        title: "Q2. The condition in an if /else statement is enclosed within:",
+        title: "The condition in an if/else statement is enclosed within:",
         choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
         answer: "Parentheses"
         
     },
     {
-        title: "Q3. Arrays in JavaScript can be used to store:",
+        title: "Arrays in JavaScript can be used to store:",
         choices: ["Numbers and Strings", "Other arrays", "Booleans", "All of the above"],
         answer: "All of the above" 
 
     },
     {
-        title: "Q4. String values must be enclosed within _______ when being assigned to variables.",
+        title: "String values must be enclosed within _______ when being assigned to variables.",
         choices: ["Commas", "Curly brackets", "Quotes", "Parentheses"],
         answer: "Quotes" 
 
     },
     {
-        title: "Q5. A very useful tool used during development and debugging for printing content to the debugger is:",
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "Terminal/Command Prompt", "For Loops", "The console.log method"],
         answer: "The console.log method" 
 
@@ -47,11 +47,11 @@ function startTimer() {
     }, 1000);
 };
 
-// Ask question
 var currentIndex = 0;
 console.log("Current Index is: " + currentIndex);
-var questionContainer = document.getElementById("question-container");
-function askQuestions() {
+
+function askQuestion() {
+    var questionContainer = document.getElementById("question-container");
     var questionTitle = questions[currentIndex].title;
     questionContainer.textContent = questionTitle;
     var choices = questions[currentIndex].choices;
@@ -61,32 +61,27 @@ function askQuestions() {
     document.getElementById("choice-4").innerHTML = choices[3];
 };
 
-// Collect user input
-var choicesContainer = document.getElementById("choices-container");
-choicesContainer.addEventListener("click", function(event) {
-    var userInput = event.target.innerHTML;
+function choiceButtonClick(btn) {
+    var userInput = btn.innerHTML;
     if(userInput == questions[currentIndex].answer) {
-        currentIndex++;
-        console.log(currentIndex);
         console.log("Correct");
     } 
-
-    else if(userInput!==questions[currentIndex].answer) {
-         currentIndex++;
-         seconds -= 10;
-         console.log(currentIndex);
-         console.log("Incorrect");
+    else if(userInput !== questions[currentIndex].answer) {
+        seconds -= 10;
+            console.log("Incorrect");
     }
 
-    // Check for quiz completion and redirect to highscores
-    if(currentIndex >= 5) {
-    window.location.href = 'highscores.html';
-    } else {
-        // Ask the next question only if the quiz is not completed
-        askQuestions();
+    // Check if all questions are complete and redirect to highscores
+    if(currentIndex == questions.length - 1) {
+        window.location.href = 'highscores.html';
     }
-});
 
-// Call functions for application
-startTimer();
-askQuestions();
+    // Ask the next question only if the quiz is not completed
+    currentIndex++;
+    askQuestion();
+};
+
+if(document.getElementById("quiz")) {
+    startTimer();
+    askQuestion();
+};
